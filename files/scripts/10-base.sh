8f4df22e5f22bede34cd1f8c3361bb550ab24695
RJ Graffham <psquid@psquid.net>
2026-02-12 11:03:54 +0000

    Create nix mountpoint only

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -2,6 +2,13 @@
 
 set -xeuo pipefail
 
+# add PackageKit to handle command-not-found
+dnf -y install \
+    packagekit
+
+# create /nix to be used as mountpoint by determinate nix installer
+mkdir -p /nix
+
 # add basic command-line tools
 dnf -y install \
     git \
