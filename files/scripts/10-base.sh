aa008a3501b6f765284b37d4b603fe0bb86347aa
RJ Graffham <psquid@psquid.net>
2026-02-12 10:15:00 +0000

    Back out nix from image, in favour of determinate nix installer at runtime

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -2,13 +2,8 @@
 
 set -xeuo pipefail
 
-# create nix root under /var and symlink to expected location
-mkdir -p /var/nix
-ln -s /var/nix /nix
-
 # add basic command-line tools
 dnf -y install \
     git \
     make \
-    just \
-    nix
+    just
