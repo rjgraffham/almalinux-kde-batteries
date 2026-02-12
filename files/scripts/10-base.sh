bd16b3b6d0a84ea98f88f339cc2b21ccd4d7c2ac
RJ Graffham <psquid@psquid.net>
2026-02-12 11:09:22 +0000

    Correct packagekit capitalization and add pk-cnf package

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -4,7 +4,8 @@ set -xeuo pipefail
 
 # add PackageKit to handle command-not-found
 dnf -y install \
-    packagekit
+    PackageKit \
+    PackageKit-command-not-found
 
 # create /nix to be used as mountpoint by determinate nix installer
 mkdir -p /nix
