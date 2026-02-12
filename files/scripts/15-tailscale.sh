8adc72e9a337cea575d4121f90dac1a7591d6c70
RJ Graffham <psquid@psquid.net>
2026-02-12 11:13:46 +0000

    Correct repo add command

--- files/scripts/15-tailscale.sh
+++ files/scripts/15-tailscale.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 # add tailscale repo and install
-dnf -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/rhel/10/tailscale.repo
+dnf -y config-manager --add-repo https://pkgs.tailscale.com/stable/rhel/10/tailscale.repo
 dnf -y install tailscale
 
 # enable tailscale service
